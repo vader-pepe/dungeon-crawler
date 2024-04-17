7c2872e2d628c841a84f001f53e19e49c510160e
vader-pepe <vader-pepe@email.com>
2024-04-18 06:45:46 +0700

    feat: render first frame of tilemap

--- Tiles.tsx
+++ Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="../../../Downloads/Pixel Crawler - FREE - 1.8/Environment/Dungeon Prison/Assets/Tiles.png" width="400" height="400"/>
+ <image source="dungeon_crawler/src/assets/img/Environment/Dungeon Prison/Assets/Tiles.png" width="400" height="400"/>
 </tileset>
--- dungeon_crawler/src/main.rs
+++ dungeon_crawler/src/main.rs
@@ -1,4 +1,8 @@
-use raylib::{ffi::Vector2, prelude::*};
+use raylib::{
+    color::Color,
+    drawing::RaylibDraw,
+    ffi::{KeyboardKey, Rectangle, Vector2},
+};
 
 const SCREEN_WIDTH: i32 = 640;
 const SCREEN_HEIGHT: i32 = 480;
@@ -9,9 +13,33 @@ fn main() {
         .title("Dungeon Crawler")
         .build();
 
+    let tile_texture = &rl
+        .load_texture(
+            &thread,
+            "./src/assets/img/Environment/Dungeon Prison/Assets/Tiles.png",
+        )
+        .expect("unable to load texture");
+
     let hero_texture = &rl
         .load_texture(&thread, "./src/assets/img/Heroes/Rogue/Idle/Idle-Sheet.png")
-        .unwrap();
+        .expect("unable to load texture");
+    let map_width: i32 = 16;
+    let map_height: i32 = 16;
+    let mut tile_dest: Rectangle = Rectangle {
+        x: 0.0,
+        y: 0.0,
+        width: 16.0,
+        height: 16.0,
+    };
+    let mut tile_src: Rectangle = Rectangle {
+        x: 0.0,
+        y: 0.0,
+        width: 16.0,
+        height: 16.0,
+    };
+    let mut tile_map: Vec<i32> = vec![0];
+    tile_map.push(99);
+    let mut src_map: Vec<&str>;
 
     let mut player_position = Vector2 { x: 0.0, y: 0.0 };
     let frame_rec = Rectangle {
@@ -71,6 +99,24 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
+        // TODO: fix this abomination
+        for (i, val) in tile_map.iter().enumerate() {
+            if *val != 0 {
+                tile_dest.x = tile_dest.width * ((i as f32) % (map_width as f32));
+                tile_dest.y = tile_dest.height * ((i as f32) % (map_height as f32));
+                tile_src.x = tile_src.width
+                    * (((*val as f32) - 1.0) % ((tile_texture.width as f32) / tile_src.width));
+                tile_src.y = tile_src.height
+                    * (((*val as f32) - 1.0) / ((tile_texture.width as f32) / tile_src.width))
+            }
+        }
+
+        d.draw_texture_rec(
+            tile_texture,
+            tile_dest,
+            Vector2 { x: 0.0, y: 0.0 },
+            Color::WHITE,
+        );
         d.draw_texture_rec(hero_texture, frame_rec, player_position, Color::WHITE);
     }
 }
