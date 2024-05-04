e1650b7033edb8b7b6732563ab2470b6b94b0a6e
vader-pepe <vader-pepe@email.com>
2024-05-04 19:01:49 +0700

    feat: update yml

---
+++ linux_pre_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get update && sudo apt-get install --asume-yes --no-install-recommends xorg-dev
---
+++ win_pre_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get update && sudo apt-get install --asume-yes --no-install-recommends libclang-10-dev clang-10 libclang-cpp10
