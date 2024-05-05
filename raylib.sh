2b72295ddaa1d7dabae4794cdefbfc4a3f13b0e7
vader-pepe <vader-pepe@email.com>
2024-05-05 08:09:14 +0700

    feat: update cicd

--- raylib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apt-get install build-essential git libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
-git clone https://github.com/raysan5/raylib.git raylib
-cd raylib/src/
-make PLATFORM=PLATFORM_DESKTOP
